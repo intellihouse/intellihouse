0914229637067f940fc2232f613186d39a645be2
Marco หงุ่ยตระกูล [Nguitragool] <marco@nightlabs.de>
2018-09-27 08:49:33 +0700

    Fixed wrong parsing of GetInverterStatus.

--- co.codewizards.raspi1/src/main/java/co/codewizards/raspi1/steca/GetInverterStatus.java
+++ co.codewizards.raspi1/src/main/java/co/codewizards/raspi1/steca/GetInverterStatus.java
@@ -34,35 +34,45 @@ public class GetInverterStatus extends StecaRequest<InverterStatus> {
 		if (fields.length != 21)
 			throw new IOException("Malformed response: " + s);
 
-		System.out.println(s);
+//		System.out.println(s);
+//
+//		for (int i = 0; i < fields.length; i++) {
+//			System.out.println(Integer.toString(i) + ": " + fields[i]);
+//		}
 
 		InverterStatus result = new InverterStatus();
 
 		int idx = -1;
-		result.setAcInVoltage(parseFloat(fields[++idx]));
-		result.setAcInFrequency(parseFloat(fields[++idx]));
-		result.setAcOutVoltage(parseFloat(fields[++idx]));
-		result.setAcOutFrequency(parseFloat(fields[++idx]));
-		result.setAcOutApparentPower(parseFloat(fields[++idx]));
-		result.setAcOutActivePower(parseFloat(fields[++idx]));
-		result.setAcOutLoadPercentage(parseFloat(fields[++idx]));
-		result.setInternalBusVoltage(parseFloat(fields[++idx]));
-		result.setBatteryVoltageAtInverter(parseFloat(fields[++idx]));
-		result.setBatteryChargeCurrent(parseFloat(fields[++idx]));
-		result.setBatteryCapacityPercentage(parseFloat(fields[++idx]));
-		result.setHeatSinkTemperature(parseFloat(fields[++idx]));
-		result.setPvToBatteryCurrent(parseInt(fields[++idx]));
-		result.setPvVoltage(parseFloat(fields[++idx]));
-		result.setBatteryVoltageAtCharger(parseFloat(fields[++idx]));
-		result.setBatteryDischargeCurrent(parseFloat(fields[++idx]));
-		result.setStatusBitmask(parseIntBinary(fields[++idx]));
-		result.setEepromVersion(parseInt(fields[++idx]));
-		result.setPvChargePower(parseFloat(fields[++idx]));
+		result.setAcInVoltage(parseFloat(fields[++idx]));                    //  0
+		result.setAcInFrequency(parseFloat(fields[++idx]));                  //  1
+		result.setAcOutVoltage(parseFloat(fields[++idx]));                   //  2
+		result.setAcOutFrequency(parseFloat(fields[++idx]));                 //  3
+		result.setAcOutApparentPower(parseFloat(fields[++idx]));             //  4
+		result.setAcOutActivePower(parseFloat(fields[++idx]));               //  5
+		result.setAcOutLoadPercentage(parseFloat(fields[++idx]));            //  6
+		result.setInternalBusVoltage(parseFloat(fields[++idx]));             //  7
+		result.setBatteryVoltageAtInverter(parseFloat(fields[++idx]));       //  8
+		result.setBatteryChargeCurrent(parseFloat(fields[++idx]));           //  9
+		result.setBatteryCapacityPercentage(parseFloat(fields[++idx]));      // 10
+		result.setHeatSinkTemperature(parseFloat(fields[++idx]));            // 11
+		result.setPvToBatteryCurrent(parseInt(fields[++idx]));               // 12
+		result.setPvVoltage(parseFloat(fields[++idx]));                      // 13
+		result.setBatteryVoltageAtCharger(parseFloat(fields[++idx]));        // 14
+		result.setBatteryDischargeCurrent(parseFloat(fields[++idx]));        // 15
+		result.setStatusBitmask(parseIntBinary(fields[++idx]));              // 16
+		++idx; // reserved field                                             // 17
+		result.setEepromVersion(parseInt(fields[++idx]));                    // 18
+		result.setPvChargePower(parseFloat(fields[++idx]));                  // 19
 
-		int statusBits2 = parseIntBinary(fields[++idx]);
+		int statusBits2 = parseIntBinary(fields[++idx]);                     // 20
 		statusBits2 = statusBits2 << 8;
 		result.setStatusBitmask(result.getStatusBitmask() | statusBits2);
 
 		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(parseInt("01084"));
+		System.out.println(parseInt("01084"));
 	}
 }
