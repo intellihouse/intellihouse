2a5bdb7476af5c73f014ccae9213861819a62b45
Marco หงุ่ยตระกูล [Nguitragool] <marco@nightlabs.de>
2018-12-31 19:39:57 +0700

    Improved cluster calculation.

--- house.intelli/org.openhab.binding.intellihouse/src/main/java/org/openhab/binding/intellihouse/rpc/pv/PvStatusClusterCalculator.java
+++ house.intelli/org.openhab.binding.intellihouse/src/main/java/org/openhab/binding/intellihouse/rpc/pv/PvStatusClusterCalculator.java
@@ -10,7 +10,7 @@ import house.intelli.core.rpc.pv.PvStatus;
 // TODO this should be configurable! Currently, it manages just 1 single cluster and assumes that all devices belong to it.
 public class PvStatusClusterCalculator {
 
-    // TODO should be a list of clusters with assignments of individual machines.
+    // TODO should be a list of clusters with assignments of individual machines -- and their names should be configurable, of course.
     protected static final String clusterDeviceName = "stecaCluster";
 
     public PvStatusClusterCalculator() {
@@ -19,50 +19,84 @@ public class PvStatusClusterCalculator {
     public List<PvStatus> calculateClusters(final List<PvStatus> currentPvStatuses) {
         requireNonNull(currentPvStatuses, "currentPvStatuses");
         final List<PvStatus> result = new ArrayList<>(1);
+        if (currentPvStatuses.isEmpty()) { // empty input => shortcut = return *empty* result.
+            return result;
+        }
 
         final PvStatus clusterPvStatus = new PvStatus();
-        if (! currentPvStatuses.isEmpty()) {
-            result.add(clusterPvStatus);
-            clusterPvStatus.setDeviceName(clusterDeviceName);
-            clusterPvStatus.setPvPower(0);
-            clusterPvStatus.setAcOutApparentPower(0);
-            clusterPvStatus.setAcOutActivePower(0);
-            clusterPvStatus.setBatteryChargeCurrent(0);
-            clusterPvStatus.setBatteryDischargeCurrent(0);
-        }
-        // TODO check what other values have to be summed up.
-        // TODO instead of the last-data-wins-strategy, maybe calculate an average or take the minimum/maximum of the values?!
+
+        // initialize aggregated result
+        clusterPvStatus.setDeviceName(clusterDeviceName);
+        clusterPvStatus.setAcInVoltage(0); // average
+        clusterPvStatus.setAcInFrequency(0); // average
+        clusterPvStatus.setAcOutVoltage(0); // average
+        clusterPvStatus.setAcOutFrequency(0); // average
+        clusterPvStatus.setAcOutApparentPower(0); // sum
+        clusterPvStatus.setAcOutActivePower(0); // sum
+        clusterPvStatus.setAcOutLoadPercentage(Float.MIN_VALUE); // max
+        clusterPvStatus.setInternalBusVoltage(0); // average
+        clusterPvStatus.setBatteryChargeCurrent(0); // sum
+        clusterPvStatus.setBatteryDischargeCurrent(0); // sum
+        clusterPvStatus.setBatteryVoltageAtCharger(Float.MAX_VALUE); // min
+        clusterPvStatus.setBatteryVoltageAtInverter(Float.MAX_VALUE); // min
+        clusterPvStatus.setBatteryCapacityPercentage(Float.MAX_VALUE); // min
+        clusterPvStatus.setHeatSinkTemperature(Float.MIN_VALUE); // max
+        clusterPvStatus.setPvToBatteryCurrent(0); // sum
+        clusterPvStatus.setPvVoltage(0); // average
+        clusterPvStatus.setPvPower(0);
+
+        int count = 0;
         for (PvStatus pvStatus : currentPvStatuses) {
             if (isCluster(pvStatus)) {
                 continue;
             }
+            ++count;
             clusterPvStatus.setMeasured(pvStatus.getMeasured());
             clusterPvStatus.setDeviceMode(pvStatus.getDeviceMode()); // maybe better strategy? or does not matter, because the cluster is always in the same state, anyway?
-            clusterPvStatus.setAcInVoltage(pvStatus.getAcInVoltage());
-            clusterPvStatus.setAcInFrequency(pvStatus.getAcInFrequency());
-            clusterPvStatus.setAcOutVoltage(pvStatus.getAcOutVoltage());
-            clusterPvStatus.setAcOutFrequency(pvStatus.getAcOutFrequency());
+            clusterPvStatus.setAcInVoltage(clusterPvStatus.getAcInVoltage() + pvStatus.getAcInVoltage());
+            clusterPvStatus.setAcInFrequency(clusterPvStatus.getAcInFrequency() + pvStatus.getAcInFrequency());
+            clusterPvStatus.setAcOutVoltage(clusterPvStatus.getAcOutVoltage() + pvStatus.getAcOutVoltage());
+            clusterPvStatus.setAcOutFrequency(clusterPvStatus.getAcOutFrequency() + pvStatus.getAcOutFrequency());
             clusterPvStatus.setAcOutApparentPower(clusterPvStatus.getAcOutApparentPower() + pvStatus.getAcOutApparentPower());
             clusterPvStatus.setAcOutActivePower(clusterPvStatus.getAcOutActivePower() + pvStatus.getAcOutActivePower());
-            clusterPvStatus.setAcOutLoadPercentage(pvStatus.getAcOutLoadPercentage());
-            clusterPvStatus.setInternalBusVoltage(pvStatus.getInternalBusVoltage());
-            clusterPvStatus.setBatteryVoltageAtInverter(pvStatus.getBatteryVoltageAtInverter());
+            clusterPvStatus.setAcOutLoadPercentage(
+                    Math.max(clusterPvStatus.getAcOutLoadPercentage(), pvStatus.getAcOutLoadPercentage()));
+            clusterPvStatus.setInternalBusVoltage(clusterPvStatus.getInternalBusVoltage() + pvStatus.getInternalBusVoltage());
+            clusterPvStatus.setBatteryVoltageAtCharger(
+                    Math.min(clusterPvStatus.getBatteryVoltageAtCharger(), pvStatus.getBatteryVoltageAtCharger()));
+            clusterPvStatus.setBatteryVoltageAtInverter(
+                    Math.min(clusterPvStatus.getBatteryVoltageAtInverter(), pvStatus.getBatteryVoltageAtInverter()));
             clusterPvStatus.setBatteryChargeCurrent(clusterPvStatus.getBatteryChargeCurrent() + pvStatus.getBatteryChargeCurrent());
-            clusterPvStatus.setBatteryCapacityPercentage(pvStatus.getBatteryCapacityPercentage());
-            clusterPvStatus.setHeatSinkTemperature(pvStatus.getHeatSinkTemperature());
-            clusterPvStatus.setPvToBatteryCurrent(pvStatus.getPvToBatteryCurrent());
-            clusterPvStatus.setPvVoltage(pvStatus.getPvVoltage()); // this should likely be an average.
-            clusterPvStatus.setBatteryVoltageAtCharger(pvStatus.getBatteryVoltageAtCharger()); // minimum? maximum? average?
+            clusterPvStatus.setBatteryCapacityPercentage(
+                    Math.min(clusterPvStatus.getBatteryCapacityPercentage(), pvStatus.getBatteryCapacityPercentage()));
+            clusterPvStatus.setHeatSinkTemperature(
+                    Math.max(clusterPvStatus.getHeatSinkTemperature(), pvStatus.getHeatSinkTemperature()));
+            clusterPvStatus.setPvToBatteryCurrent(clusterPvStatus.getPvToBatteryCurrent() +pvStatus.getPvToBatteryCurrent());
+            clusterPvStatus.setPvVoltage(clusterPvStatus.getPvVoltage() + pvStatus.getPvVoltage());
             clusterPvStatus.setBatteryDischargeCurrent(clusterPvStatus.getBatteryDischargeCurrent() + pvStatus.getBatteryDischargeCurrent());
             clusterPvStatus.setStatusBitmask(pvStatus.getStatusBitmask());
             clusterPvStatus.setEepromVersion(pvStatus.getEepromVersion());
             clusterPvStatus.setPvPower(clusterPvStatus.getPvPower() + pvStatus.getPvPower());
+
         }
+        if (count <= 0) { // none of the list was usable => return *empty* result.
+            return result;
+        }
+
+        // average divions:
+        clusterPvStatus.setAcInVoltage(clusterPvStatus.getAcInVoltage() / count);
+        clusterPvStatus.setAcInFrequency(clusterPvStatus.getAcInFrequency() / count);
+        clusterPvStatus.setAcOutVoltage(clusterPvStatus.getAcOutVoltage() / count);
+        clusterPvStatus.setAcOutFrequency(clusterPvStatus.getAcOutFrequency() / count);
+        clusterPvStatus.setInternalBusVoltage(clusterPvStatus.getInternalBusVoltage() / count);
+        clusterPvStatus.setPvVoltage(clusterPvStatus.getPvVoltage() / count);
+
+        result.add(clusterPvStatus);
         return result;
     }
 
     /**
-     * Is the PV-status of a cluster -- not a member-device.
+     * Is the PV-status of a cluster -- not of a member-device.
      * @param pvStatus
      * @return
      */
